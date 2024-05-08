c3a9f752c31ce465d15566528d1bb3fdf8d719ec
Aditi <aditideshpande2908@gmail.com>
2024-05-08 16:18:51 +0530

    Set daily limit for each floor

--- src/app.js
+++ src/app.js
@@ -1,14 +1,37 @@
+let totalCost = 0
+
 document.addEventListener('DOMContentLoaded', function () {
-	generateCalendar('Ground')
-	generateCalendar('First')
-	generateCalendar('Second')
+	generateCalendar('Ground', 1)
+	generateCalendar('First', 2)
+	generateCalendar('Second', 1.5)
 })
 
-function generateCalendar(floorName) {
+function generateCalendar(floorName, dailyLimit) {
+	const milkAmountPerLitre = 48
 	let calendarEl = document.getElementById(`calendar${floorName}`)
 	let calendar = new FullCalendar.Calendar(calendarEl, {
 		initialView: 'dayGridMonth',
 	})
 
 	calendar.render()
+	calendar.on('dateClick', function (info) {
+		console.log('clicked on ' + info.dateStr)
+		let currentColor = info.dayEl.style.backgroundColor
+
+		let colorGreen = 'rgb(151, 173, 109)'
+		let colorDefault = 'rgb(255, 255, 255)'
+
+		if (currentColor === '' || currentColor === colorDefault) {
+			info.dayEl.style.backgroundColor = colorGreen
+		} else if (currentColor === colorGreen) {
+			info.dayEl.style.backgroundColor = colorDefault
+		}
+	})
+
+	const currentDate = moment()
+	const daysInMonth = currentDate.daysInMonth()
+	console.log('Number of days in the current month:', daysInMonth)
+
+	let costOfCurrentMonth = daysInMonth * dailyLimit * milkAmountPerLitre
+	console.log(costOfCurrentMonth)
 }
